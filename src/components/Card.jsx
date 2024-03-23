e6f22b38a180165c68ea2e00d253582dc08ac2d9
Xyves <xodrixx@protonmail.com>
2024-03-23 22:24:34 +0100

    Add object destructuring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,15 @@
 import { useEffect } from "react";
-export default function Card({handleClick,name,imgUrl}) {
-    useEffect(()=>{
-      const character=
-    fetch()
-    return(
-
-    )
-  })
+export default function Card({ num, id, name }) {
+  const handleClick = () => {
+    console.log(num);
+  };
   return (
     <div className="card" onClick={handleClick}>
       <div className="image">
         <img src="" alt="" />
       </div>
       <div className="textContainer">
-        <h2></h2>
+        <h2>{name}</h2>
       </div>
     </div>
   );
