93e1fc4cd12c2ace8feeb7884f238a3530d3bd0c
Xyves <xodrixx@protonmail.com>
2024-03-25 22:05:27 +0100

    Implement shuffling cards and fetching API

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,34 +2,75 @@ import { v4 as uuidv4 } from "uuid";
 import { useEffect, useState, useRef } from "react";
 import Card from "./Card";
 export default function Main() {
+  const [cards, setCards] = useState(initialState);
   const initialState = new Array(8).fill({
     num: calculateNum(),
     name: "",
     id: uuidv4(),
     src: "",
   });
-  const updateCards = () => {
-    useEffect(() => {
-      fetch();
-    });
-    const newArray = new Array(8).fill({}).map(() => ({
-      num: calculateNum(),
-      name: "",
-      id: uuidv4(),
-      src: "",
-    }));
-    setCards(newArray);
-  };
+  // Initial load
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const updatedCards = await Promise.all(
+          cards.map(async (card) => {
+            const response = await fetch(
+              `https://gravity-falls-api.vercel.app/api/characters/${card.num}`
+            );
+            const data = await response.json();
+            return { ...card, name: data.name, src: data.src };
+          })
+        );
+        setCards(updatedCards);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const updatedCards = await Promise.all(
+          cards.map(async (card) => {
+            const response = await fetch(
+              `https://api.example.com/data?num=${card.num}`
+            );
+            const data = await response.json();
+            return { ...card, name: data.name, src: data.src };
+          })
+        );
+        setCards(updatedCards);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [cards]);
+
   const calculateNum = () => {
     return Math.floor(Math.random() * 143) + 1;
   };
-  const shuffle = () => {};
-  const [cards, setCards] = useState(initialState);
+
+  // Fisher–Yates shuffle
+  const shuffle = () => {
+    const shuffledArray = [...cards];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    setCards(shuffledArray);
+  };
   return (
     <main>
       <div className="cardContainer">
-        {cards.map((num, index) => (
-          <Card key={index} shuffle={shuffle} number={num} />
+        {cards.map((num, index, name) => (
+          <Card key={index} number={num} name={name} shuffle={shuffle} />
         ))}
       </div>
     </main>
